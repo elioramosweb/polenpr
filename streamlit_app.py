2ce81517a320c09da42222f4e8c30377e43da60d
elioramosweb <97993994+elioramosweb@users.noreply.github.com>
2022-06-06 14:29:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,11 @@ import os
 import pickle
 import requests
 from io import BytesIO
+from pathlib import Path
 
 
 
-#st.set_page_config(page_title="polen",layout="wide")
+st.set_page_config(page_title="polen",layout="wide")
 
 ######################################
 ## suprimir los warnings de tensorflow
@@ -106,8 +107,9 @@ def crop_max_square(pil_img):
 ## titulo
 ######### 
 
-st.title("Clasificador de Polen utilizando un Modelo de Redes Neuronales e Inteligencia Artificial")
+st.title("Clasificador de Polen")
 
+st.subheader("Utilizando un Modelo de Redes Neuronales Convolucionales")
 
 ## cargar modelo de clasificación 
 
@@ -122,25 +124,44 @@ st.sidebar.markdown("<hr>",unsafe_allow_html=True)
 
 col1, col2 = st.columns(2)
 
+############################
+## checkbox de mostrar texto 
+############################ 
+
+mostrarTexto = st.sidebar.checkbox('Mostrar texto informátivo',value=True)
+
+################################
+## checkbox de mostrar imágenes
+################################ 
+
+mostrarImagen = st.sidebar.checkbox('Mostrar imágenes',value=True)
+
+#############################
+## checkbox de mostrar barras
+#############################
+
+mostrarBarra = st.sidebar.checkbox('Mostrar Gráfico Barras',value=True)
+
+############################
+## checkbox de mostrar tabla
+############################ 
+
+mostrarTabla = st.sidebar.checkbox('Mostrar Tabla con clasificación',value=True)
+
+st.sidebar.markdown("<hr>",unsafe_allow_html=True)
+
 ###############
 ## SUBIR IMAGEN
 ############### 
 
 
-st.sidebar.markdown("<b><font color='#f63366'>Subir imagen que se quiere clasificar...</font></b>",unsafe_allow_html=True)
-image = st.sidebar.file_uploader("", type=["png", "jpg", "jpeg"])
+st.sidebar.markdown("<b><font color='#f63366'>Subir imagen o imágenes que se quiere clasificar...</font></b>",unsafe_allow_html=True)
 
+with st.form("my-form", clear_on_submit=True):
+    images = st.sidebar.file_uploader("", type=["png", "jpg", "jpeg"],accept_multiple_files=True)
 
 st.sidebar.markdown("<hr>",unsafe_allow_html=True)
 
-
-##############################################################
-## SLIDER PARA FIJAR CANTIDAD DE ESPECIES EN GRAFICO DE BARRAS
-############################################################## 
-
-st.sidebar.markdown("<b><font color='#f63366'>Cantidad de especies para el gráfico de barras</font></b>",unsafe_allow_html=True)
-cantEspecies = st.sidebar.slider('',
-                         min_value=1,max_value=10,value=10,step=1)
 
 
 ####################################
@@ -153,88 +174,142 @@ class_temp = list(pickle.load(file).keys())
 
 class_names = [val.replace("_","") for val in class_temp]
 
-if image is not None:
-    
-    st.markdown("<hr>",unsafe_allow_html=True)
 
-    imageOriginal = load_image(image)
+if mostrarTexto:
 
-    imageCrop = crop_max_square(imageOriginal)
-
-    imageResize = ImageOps.grayscale(imageCrop).resize((50,50))
-
-    col1.image(imageOriginal,width=250,caption='Imagen Original')
-    col2.image(imageResize,width=250,caption='Imagen Gris')
-
-    probs,classPredict = predictImage(reshapeImage(imageResize))
-
-
-    ## DATAFRAME ##
-
-    df = pd.DataFrame(list(zip(class_names, probs[0])),
-               columns =['especie', 'probabilidad'])
-
-    df = df.sort_values(by='probabilidad', ascending=False).head(cantEspecies)
-
-    df["probabilidad"] *= 100
-
-    ## GRAFICO DE BARRAS 
-
-    fig = prob_barplot(df)
-    
-    st.plotly_chart(fig,width=200)
-
-    st.markdown("<b>Clasificación por especie</b>",unsafe_allow_html=True)
-    
+    st.markdown("<b><font color='#f63366'>Clasificación por especie</font></b>",unsafe_allow_html=True)
     st.markdown("<p>Se muestran las <b>cinco posibles especies </b> de un total de <b>85</b> basadas en\
                 la probabilidad que produce el modelo. Si la probabilidad mayor\
                 es cercana a 100 las restantes imágenes podrían ser clasificaciones dudosas.</p>",
                 unsafe_allow_html=True) 
-                
 
-    col = {}
 
-    col[0],col[1],col[2],col[3],col[4] = st.columns(5)
+if mostrarBarra:
+    st.sidebar.markdown("<b><font color='#f63366'>Cantidad de especies para el gráfico de barras</font></b>",unsafe_allow_html=True)
+    cantEspecies = st.sidebar.slider('',min_value=1,max_value=10,value=10,step=1)
+
+listaDF = []
+
+if images is not None:
+
+    my_bar = st.progress(0)
+
+    largo = len(images)
+
+    inx = 1
+
+    for image in images:
+
+        ## ajusstar dimensiones de imagen de entrada 
+
+        imageOriginal = load_image(image)
     
-    ##gitPath = "https://github.com/elioramosweb/plantasOne/blob/main/"
-
-    ##gitPath = "https://github.com/elioramosweb/plantasOne/blob/main/a14_1.jpg?raw=true"
-
-    gitPath= "https://raw.githubusercontent.com/elioramosweb/plantasOne/main/"
-  
-
-    # IMAGENES EN COLORES 
-
-
-    for i in range(5):
-
-        temp = str(list(df["especie"])[i])
-
-        url = gitPath + temp + "_1.jpg"
-
-        page = requests.get(url)
-
-        fig2 = Image.open(BytesIO(page.content))
-
-        #fig2 = load_image(glob.glob("./Plantas/" + temp + "/*.jpg")[0])
-
-        col[i].image(fig2,width=100,caption=temp)
-   
-    # IMAGENES EN ESCALA DE GRIS 
-
-    for i in range(5):
-
-        temp = str(list(df["especie"])[i])
-
-        url = gitPath + temp + "_1.jpg"
-
-        page = requests.get(url)
-
-        fig2 = Image.open(BytesIO(page.content))
-
-        col[i].image(ImageOps.grayscale(fig2),width=100,caption=temp + "(Gris)")
-
-else:
+        imageCrop = crop_max_square(imageOriginal)
     
-    st.markdown("<h5>Una vez suba la imagen los resultados van a aparecer aquí....</h5>",unsafe_allow_html=True)
+        imageResize = ImageOps.grayscale(imageCrop).resize((50,50))
     
+        ## hacer predicción con el modelo 
+
+        probs,classPredict = predictImage(reshapeImage(imageResize))
+    
+        ## DATAFRAME con resultados ##
+    
+        df = pd.DataFrame(list(zip(class_names, probs[0])),
+                   columns =['especie', 'probabilidad'])
+    
+        ## slider para cantidad de barras 
+
+        cantEspecies = 10
+
+        df = df.sort_values(by='probabilidad', ascending=False).head(cantEspecies)
+    
+        df["probabilidad"] *= 100
+        
+    
+        ## GRAFICO DE BARRAS 
+    
+        if mostrarBarra:
+            fig = prob_barplot(df)
+            st.plotly_chart(fig,width=200)
+    
+
+        lista1 = list(df["especie"])
+        lista2 = list(df["probabilidad"])
+                      
+        temp = {lista1[i]:[lista2[i]] for i in range(5)}
+
+        df2 = pd.DataFrame(temp).rename({0:'probabilidad'}).style.format("{:.2f}")
+
+        col = {}
+    
+        col[0],col[1],col[2],col[3],col[4],col[5] = st.columns(6)
+        
+        gitPath= "https://raw.githubusercontent.com/elioramosweb/plantasOne/main/"
+      
+        # IMAGENES EN COLORES 
+    
+        if mostrarImagen:
+
+            col[0].image(imageOriginal,width=100,caption=image.name)
+    
+        listaImagenes = []
+
+        listaImagenes.append(image.name)
+    
+        for i in range(5):
+    
+            tempEspecie = str(list(df["especie"])[i])
+           
+            tempProb = str(round(list(df["probabilidad"])[i],2))
+    
+            url = gitPath + tempEspecie + "_1.jpg"
+    
+            page = requests.get(url)
+    
+            fig2 = Image.open(BytesIO(page.content))
+
+            imageCaption = tempEspecie + " - " + tempProb + "%"
+
+            listaImagenes.append(imageCaption)
+
+            if mostrarImagen:
+                col[i+1].image(fig2,width=100,caption=imageCaption)
+       
+        my_bar.progress(round(100*(inx/largo)))
+
+        inx += 1
+
+        listaDF.append(listaImagenes)
+    
+
+    ## creacion de archivo de salida 
+
+    
+    if len(listaDF) > 0:
+
+        dfOutput = pd.DataFrame(listaDF)
+
+        dfOutput.columns = ['imagen','p1','p2','p3','p4','p5']
+        
+        dfOutput = dfOutput.sort_values(by='imagen')
+
+        dfOutput = dfOutput.drop_duplicates( keep='last')
+
+        if mostrarTabla:
+            st.markdown("<b><font color='#f63366'>Tabla de resultados</font></b>",unsafe_allow_html=True)
+            st.dataframe(dfOutput.sort_index())
+
+        @st.cache
+        def convert_df(df):
+            return df.to_csv().encode('utf-8')
+
+        if mostrarTabla:
+
+            csv = convert_df(dfOutput)
+            st.download_button(
+            "Presione para bajar tabla de resultados en CSV",
+            csv,
+            "resultados.csv",
+            "text/csv",
+            key='download-csv'
+            )
